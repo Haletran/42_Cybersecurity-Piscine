ab26268c953122184810b5d00bd43a86b1c7b279
Haletran <bpasquier123@gmail.com>
2025-02-03 18:54:55 +0100

    feat: implement image extraction and download functionality with progress bar, need to do the flags

--- day00/spider.go
+++ day00/spider.go
@@ -10,6 +10,7 @@ import (
     "github.com/gocolly/colly"
     "net/url"
     "path/filepath"
+    "github.com/schollz/progressbar/v3"
 )
 
 var CLI struct {
@@ -18,6 +19,48 @@ var CLI struct {
     Recursive bool `short:"r" help:"Recursively download images."`
     Links     int `short:"l" help:"Indicates the maximum depth level of the recursive download (default: 5)"`
     Pictures  string `short:"p" help:"Path where the downloaded files will be saved. (default: /data)"`
+}
+
+
+func extractImgs(link string, folder string, base_url string) {
+     imgURL, err := url.Parse(link)
+     if err != nil {
+         fmt.Println("Invalid URL:", link, err)
+         return
+     }
+     
+     if !imgURL.IsAbs() {
+         baseURL, _ := url.Parse(base_url)
+         imgURL = baseURL.ResolveReference(imgURL)
+     }
+ 
+     resp, err := http.Get(imgURL.String())
+     if err != nil {
+         fmt.Println("Error downloading:", imgURL, err)
+         return 
+     }
+     defer resp.Body.Close()
+ 
+     parts := strings.Split(imgURL.Path, "/")
+     filename := parts[len(parts)-1]
+     err = os.MkdirAll(folder, 0755)
+     if err != nil {
+         fmt.Println("Error creating directory:", err)
+         return 
+     }
+ 
+     file, err := os.Create(filepath.Join(folder, filename))
+     if err != nil {
+         fmt.Println("Error creating the file:", err)
+        return         
+     }
+     defer file.Close()
+ 
+     _, err = io.Copy(file, resp.Body)
+     if err != nil {
+         fmt.Println("Error saving file:", err)
+        return         
+     }
 }
 
 
@@ -50,60 +93,41 @@ func main() {
 
     links := []string{}
 
-    c.OnRequest(func(r *colly.Request) {
-        fmt.Println("Visiting", r.URL)
-    })
-
+    wrongFormat := 0
     c.OnHTML("img", func(e *colly.HTMLElement) {
         link := e.Attr("src")
+        format := [5]string{".png", ".gif", ".jpg", ".bmp", ".jpeg"}
+        hasValidFormat := false
+        for i := 0; i < len(format); i++ {
+            if strings.HasSuffix(strings.ToLower(link), format[i]) {
+                hasValidFormat = true
+                break
+            }
+        }
+        if !hasValidFormat {
+            wrongFormat++
+            return
+        }
         links = append(links, link)
     })
     c.Visit(CLI.URL)
     c.Wait()
 
-    fmt.Printf("Found %d links\n", len(links));
+    fmt.Println("=== Spider Results ===")
+    fmt.Printf("Total images found: %d\n", len(links))
+    fmt.Printf("Images with invalid format: %d\n", wrongFormat)
+    fmt.Printf("Saving to directory: %s\n", CLI.Pictures)
+    fmt.Println("====================")
+    
+    bar := progressbar.NewOptions(len(links),
+        progressbar.OptionSetDescription("Downloading images..."),
+        progressbar.OptionShowCount(),
+    )
+    
     for i := 0; i < len(links); i++ {
-        // Resolve relative URLs to absolute URLs
-        imgURL, err := url.Parse(links[i])
-        if err != nil {
-            fmt.Println("Invalid URL:", links[i], err)
-            continue
-        }
-        
-        // If imgURL is relative, resolve against base URL
-        if !imgURL.IsAbs() {
-            baseURL, _ := url.Parse(CLI.URL)
-            imgURL = baseURL.ResolveReference(imgURL)
-        }
-    
-        resp, err := http.Get(imgURL.String())
-        if err != nil {
-            fmt.Println("Error downloading:", imgURL, err)
-            continue
-        }
-        defer resp.Body.Close()
-    
-        parts := strings.Split(imgURL.Path, "/")
-        filename := parts[len(parts)-1]
-        err = os.MkdirAll(CLI.Pictures, 0755)
-        if err != nil {
-            fmt.Println("Error creating directory:", err)
-            continue
-        }
-    
-        file, err := os.Create(filepath.Join(CLI.Pictures, filename))
-        if err != nil {
-            fmt.Println("Error creating the file:", err)
-            continue
-        }
-        defer file.Close()
-    
-        _, err = io.Copy(file, resp.Body)
-        if err != nil {
-            fmt.Println("Error saving file:", err)
-            continue
-        }
-    
-        fmt.Printf("Downloaded: %s\n", imgURL)
+        bar.Add(1)
+        extractImgs(links[i], CLI.Pictures, CLI.URL)
     }
+    
+    fmt.Printf("\n\n✓ Successfully downloaded %d images to %s\n", len(links), CLI.Pictures)
 }
